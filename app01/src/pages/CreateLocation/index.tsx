b1670eb1a7a5e940fd6aa5f0054562d24d3cd2ef
Renato Janser <renato.janser@gmail.com>
2021-06-19 17:32:30 -0300

    Continuação da pagina create location

--- app01/src/pages/CreateLocation/index.tsx
+++ app01/src/pages/CreateLocation/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
@@ -23,13 +24,65 @@ const CreateLocation: React.FC = () => {
                         <legend>
                             <h2>Dados</h2>
                         </legend>
+                        <div className="field">
+                            <label htmlFor="name">Nome da entidade</label>
+                            <input 
+                                type="text" 
+                                name="name"
+                                id="name"
+                            />
+                        </div>
+                        <div className="field-group">
+                            <div className="field">
+                                <label htmlFor="email">E-mail</label>
+                                <input 
+                                    type="email" 
+                                    name="email"
+                                    id="email"
+                                />
+                            </div>
+                            <div className="field">
+                                <label htmlFor="whatsapp">E-mail</label>
+                                <input 
+                                    type="text" 
+                                    name="whatsapp"
+                                    id="whatsapp"
+                                />
+                            </div>
+                        </div>
                     </fieldset>
-
+                    
+                    
                     <fieldset>
                         <legend>
                             <h2>Endereço</h2>
                             <span>Marque o endereço no mapa</span>
                         </legend>
+                        <Map center={[-23.0003709, -43.365895]} zoom={14}>
+                            <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[-23.0003709, -43.365895]} />
+                        </Map>
+                        <div className="field-group">
+                            <div className="field">
+                                <label htmlFor="city">Cidade</label>
+                                <input 
+                                    type="text" 
+                                    name="city"
+                                    id="city"
+                                />
+                            </div>
+                            <div className="field">
+                                <label htmlFor="uf">Estado</label>
+                                <input 
+                                    type="text" 
+                                    name="uf"
+                                    id="uf"
+                                />
+                            </div>
+                        </div>
                     </fieldset>
 
                     <fieldset>
@@ -37,6 +90,17 @@ const CreateLocation: React.FC = () => {
                             <h2>Itens coletaos</h2>
                             <span>Você pode marcar um ou mais itens</span>
                         </legend>
+                        <ul className="items-grid">
+                            <li>
+                                <img src="" alt="" />
+                            </li>
+                            <li className="selected">
+                                <img src="" alt="" />
+                            </li>
+                            <li>
+                                <img src="" alt="" />
+                            </li>
+                        </ul>
                     </fieldset>
 
                     <button type="submit">
